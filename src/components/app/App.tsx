1f1056754be75e24f0ccc028955e863e0cdd1b1c
Costele <costelerm@gmail.com>
2021-01-26 10:39:03 +0200

    123

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Text1 } from "../demo/Text1";
 import { Text2 } from "../demo/Text2";
 import { Text3 } from "../demo/Text3";
-import { Countries } from "../demo/countries/Countries";
+import { Table } from "../product/table/Table";
 
 const client = new ApolloClient({
   uri: "https://countries.trevorblades.com/",
@@ -17,7 +17,7 @@ function App() {
       <ApolloProvider client={client}>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={Countries} />
+            <Route exact path="/" component={Table} />
             <Route exact path="/text1" component={Text1} />
             <Route exact path="/text2" component={Text2} />
             <Route exact path="/text3/:id?/:page?" component={Text3} />
